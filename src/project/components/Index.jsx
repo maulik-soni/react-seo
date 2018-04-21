e6d6792ccbf28815d8266b26b713e2a4af4350db
maulik-edc <mauliksoni37@gmail.com>
2018-04-22 02:25:49 +0530

    Added Index Page Compoent

---
+++ src/project/components/Index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+   render() {
+      return (
+        <div>
+          <h2>Index</h2>
+        </div>
+      );
+   }
+}
+export default Index;
